4c9cc9550aa041a0428150087d0b199809adae12
widyaprms <arini.widya@ui.ac.id>
2023-09-20 15:24:47 +0700

    Sudah selesai mengerjakan tutorial 3

--- main/models.py
+++ main/models.py
@@ -1,8 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+
 # Create your models here.
 
-from django.db import models
-
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,9 @@ from main.views import show_main, create_product
 from main.views import show_main, create_product, show_xml
 from main.views import show_main, create_product, show_xml, show_json
 from main.views import show_main, create_product, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import register 
+from main.views import login_user 
+from main.views import logout_user
 
 app_name = 'main'
 
@@ -14,4 +17,7 @@ urlpatterns = [
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('register/', register, name='register'), 
+    path('login/', login_user, name='login'), 
+    path('logout/', logout_user, name='logout'),
 ]
